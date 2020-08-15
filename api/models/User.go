1349b0ac3c9ebe274a1577a3f822ea5a94e39116
singhayushh <coding.ayush@gmail.com>
2020-08-15 20:58:01 +0530

    User schema updated

--- api/models/User.go
+++ api/models/User.go
@@ -3,7 +3,6 @@ package models
 import (
 	"errors"
 	"html"
-	"log"
 	"strings"
 	"time"
 
@@ -12,14 +11,15 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-// User model
+// User schema
 type User struct {
-	ID        uint64    `gorm:"primary_key;auto_increment" json:"id"`
-	Type      string    `gorm:"size:15;default:'Customer'" json:"type"` // Either an Admin or Customer(by default)
-	Name      string    `gorm:"size:255;not null;" json:"name"`
-	Email     string    `gorm:"size:100;not null;unique" json:"email"`
-	Password  string    `gorm:"size:100;not null;" json:"password"`
-	Address   string    `gorm:"default:'Blank'" json:"address"`
+	ID        uint64    `gorm:"primary_key;auto_increment" json:"id" form:"id"`
+	Type      string    `gorm:"size:15;default:'Customer'" json:"type" form:"type"` // Either an Admin or Customer(by default)
+	Name      string    `gorm:"size:255;not null;" json:"name" form:"name"`
+	Email     string    `gorm:"size:100;not null;unique" json:"email" form:"email"`
+	Password  string    `gorm:"size:100;not null;" json:"password" form:"password"`
+	Image     string    `gorm:"default:'https://raw.githubusercontent.com/Simulacra-Technologies/teastore/master/templates/profile.png'" json:"image" form:"image"`
+	Address   string    `gorm:"default:'Blank'" json:"address" form:"address"`
 	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
 	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
@@ -34,24 +34,26 @@ func VerifyPassword(hashedPassword, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 }
 
-// Validate function is a utility function that implements password hashing, updating the time entries etc and checks if entries are valid, based on actions being used for (login, update etc)
-func (user *User) Validate(action string) error {
+// EncryptPassword uses the hash function to hash the user's password
+func (user *User) EncryptPassword() error {
 	hashedPassword, err := Hash(user.Password)
 	if err != nil {
 		return err
 	}
-	user.ID = 0 // gets auto set anyway
-	user.Type = strings.TrimSpace(user.Type)
+	user.Password = string(hashedPassword)
+	return nil
+}
+
+// Validate function is a utility function that implements password hashing, updating the time entries etc and checks if entries are valid, based on actions being used for (login, update etc)
+func (user *User) Validate(action string) error {
+
 	user.Name = strings.TrimSpace(user.Name)
 	user.Email = html.EscapeString(strings.TrimSpace(user.Email))
-	user.Password = string(hashedPassword)
+	user.Image = html.EscapeString(strings.TrimSpace(user.Image))
 	user.Address = strings.TrimSpace(user.Address)
 
 	switch strings.ToLower(action) {
 	case "login":
-		if user.Type == "" {
-			return errors.New("API: Type is required")
-		}
 		if user.Email == "" {
 			return errors.New("Email is required")
 		}
@@ -66,20 +68,21 @@ func (user *User) Validate(action string) error {
 		if user.Name == "" {
 			return errors.New("Name is required")
 		}
-		if user.Address == "" {
-			return errors.New("Address is required")
-		}
 		if user.Email == "" {
 			return errors.New("Email is required")
-		}
-		if user.Password == "" {
-			return errors.New("Password is required")
 		}
 		if err := checkmail.ValidateFormat(user.Email); err != nil {
 			return errors.New("Invalid Email")
 		}
+		if user.Address == "" {
+			return errors.New("Address is required")
+		}
+		if user.Image == "" {
+			user.Image = "https://raw.githubusercontent.com/Simulacra-Technologies/teastore/master/templates/profile.png"
+		}
 		return nil
 	default:
+		user.ID = 0 // gets auto set anyway
 		if user.Name == "" {
 			return errors.New("Name is required")
 		}
@@ -110,19 +113,19 @@ func (user *User) Save(db *gorm.DB) (*User, error) {
 	return user, nil
 }
 
-// FindAllUsers returns an array of User type listing upto 100 users
-func (user *User) FindAllUsers(db *gorm.DB) (*[]User, error) {
+// FetchAll returns an array of Users listing upto 100 users
+func (user *User) FetchAll(db *gorm.DB) (*[]User, error) {
 	var err error
 	users := []User{}
-	err = db.Debug().Model(&User{}).Limit(100).Find(&users).Error
+	err = db.Debug().Model(&User{}).Find(&users).Error
 	if err != nil {
 		return nil, err
 	}
 	return &users, err
 }
 
-// FindUserByID needs a uint64 uid to search for the corresponding user
-func (user *User) FindUserByID(db *gorm.DB, uid uint64) (*User, error) {
+// FetchByID needs a uint64 uid to search for the corresponding user
+func (user *User) FetchByID(db *gorm.DB, uid uint64) (*User, error) {
 	var err error
 	err = db.Debug().Model(User{}).Where("id = ?", uid).Take(&user).Error
 	if err != nil {
@@ -134,23 +137,23 @@ func (user *User) FindUserByID(db *gorm.DB, uid uint64) (*User, error) {
 	return user, err
 }
 
-// UpdateAUser currently allows changing the name, email, password and address
-func (user *User) UpdateAUser(db *gorm.DB, uid uint32) (*User, error) {
+// Update requires user auth. Allowed - name, email, image, address
+func (user *User) Update(db *gorm.DB, uid uint64) (*User, error) {
 
 	// To hash the password
 	err := user.Validate("update")
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	// Update the user
 	db = db.Debug().Model(&User{}).Where("id = ?", uid).Take(&User{}).UpdateColumns(
 		map[string]interface{}{
-			"name":      user.Name,
-			"email":     user.Email,
-			"password":  user.Password,
-			"address":   user.Address,
-			"update_at": time.Now(),
+			"name":       user.Name,
+			"email":      user.Email,
+			"Image":      user.Image,
+			"address":    user.Address,
+			"updated_at": time.Now(),
 		},
 	)
 	if db.Error != nil {
@@ -165,8 +168,8 @@ func (user *User) UpdateAUser(db *gorm.DB, uid uint32) (*User, error) {
 	return user, nil
 }
 
-// DeleteAUser is my favourite. Every user should use this.
-func (user *User) DeleteAUser(db *gorm.DB, uid uint64) (int64, error) {
+// Delete is my favourite. Every user should use this.
+func (user *User) Delete(db *gorm.DB, uid uint64) (int64, error) {
 
 	db = db.Debug().Model(&User{}).Where("id = ?", uid).Take(&User{}).Delete(&User{})
 
