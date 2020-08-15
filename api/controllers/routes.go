b513f3d524f3ac531512b3a9ebe2d57bc19c7b53
singhayushh <coding.ayush@gmail.com>
2020-08-15 20:56:23 +0530

    Endpoints added

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -5,8 +5,31 @@ import "teastore/api/middlewares"
 func (server *Server) initRoutes() {
 	server.Router.GET("/ping", Ping)
 	server.Router.GET("/", RenderHome)
-	server.Router.POST("/u/register", server.Register)
-	server.Router.POST("/u/login", server.Login)
 
-	server.Router.GET("/profile", middlewares.AuthenticationMiddleware(), RenderProfile)
+	userRoute := server.Router.Group("/users")
+	{
+		userRoute.GET("/register", RenderRegister)
+		userRoute.GET("/login", RenderLogin)
+		userRoute.GET("/view/:id", server.ShowUser)
+		userRoute.POST("/register", middlewares.PasserMiddleware(), server.Register)
+		userRoute.POST("/login", middlewares.PasserMiddleware(), server.Login)
+		userRoute.POST("/edit", middlewares.AuthenticationMiddleware(""), server.UpdateUser)
+		userRoute.POST("/delete", middlewares.AuthenticationMiddleware(""), server.DeleteUser)
+	}
+
+	productRoute := server.Router.Group("/products")
+	{
+		productRoute.GET("/view/:path", server.ShowProduct)
+		productRoute.POST("/add", middlewares.AuthenticationMiddleware("admin"), server.AddProduct)
+		productRoute.POST("/edit", middlewares.AuthenticationMiddleware("admin"), server.UpdateProduct)
+		productRoute.POST("/delete", middlewares.AuthenticationMiddleware("admin"), server.DeleteProduct)
+	}
+
+	blogRoute := server.Router.Group("/blogs")
+	{
+		blogRoute.GET("/view/:path", server.ReadBlog)
+		blogRoute.POST("/create", middlewares.AuthenticationMiddleware("admin"), server.CreateBlog)
+		blogRoute.POST("/edit", middlewares.AuthenticationMiddleware("admin"), server.UpdateBlog)
+		blogRoute.POST("/delete", middlewares.AuthenticationMiddleware("admin"), server.DeleteBlog)
+	}
 }
