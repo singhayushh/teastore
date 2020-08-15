e404fac79dbc7ec839804409c6f7e0456068609b
singhayushh <coding.ayush@gmail.com>
2020-08-15 20:57:10 +0530

    Handler for CRUD Users

--- api/controllers/user.go
+++ api/controllers/user.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"fmt"
+	"strconv"
 	"teastore/api/auth"
 	"teastore/api/models"
 
@@ -14,15 +16,25 @@ func (server *Server) Register(c *gin.Context) {
 
 	if err := c.ShouldBindJSON(&user); err != nil {
 		c.JSON(500, gin.H{"error": err})
+		fmt.Println(user)
 		return
 	}
 
+	// Check if all parameters have been inputted
 	err = user.Validate("")
 	if err != nil {
 		c.JSON(500, gin.H{"error": err})
 		return
 	}
 
+	// Password Hashing
+	err = user.EncryptPassword()
+	if err != nil {
+		c.JSON(500, gin.H{"error": err})
+		return
+	}
+
+	// _ to be changed and used for page rendering
 	_, err = user.Save(server.DB)
 
 	if err != nil {
@@ -39,26 +51,29 @@ func (server *Server) Login(c *gin.Context) {
 	var err error
 
 	if err := c.ShouldBindJSON(&user); err != nil {
-		c.JSON(500, gin.H{"error1": err})
+		c.JSON(500, gin.H{"error": err})
+		fmt.Println(err)
 		return
 	}
 
+	err = user.Validate("login")
 	if err != nil {
-		c.JSON(500, gin.H{"error2": err})
+		c.JSON(500, gin.H{"error": err})
+		fmt.Println(err)
 		return
 	}
 
 	sessionID, err := server.SignIn(user.Email, user.Password)
 
 	if err != nil {
-		c.JSON(200, gin.H{"message": "failed"})
+		c.JSON(206, gin.H{"message": "failed"})
 	} else {
 		c.SetCookie("session_id", sessionID, 7.884e+6, "/", "", false, false)
-		c.JSON(200, gin.H{"message": "success"})
+		c.Redirect(301, "/")
 	}
 }
 
-// SignIn finds user via mail, checks his password and calls auth.CreateSession()
+// SignIn is a utility function used by Login() which finds user via mail, checks his password and calls auth.CreateSession()
 func (server *Server) SignIn(email, password string) (string, error) {
 	var err error
 
@@ -76,5 +91,72 @@ func (server *Server) SignIn(email, password string) (string, error) {
 		return "", err
 	}
 
-	return auth.CreateSession(user.Email)
+	return auth.CreateSession(user.ID, user.Type)
+}
+
+// ShowUser fetches data of the user by his id
+func (server *Server) ShowUser(c *gin.Context) {
+	uid, err := strconv.ParseUint(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err})
+		return
+	}
+	fmt.Println("uid", uid)
+	user := models.User{}
+	fetchedUser, err := user.FetchByID(server.DB, uid)
+	c.JSON(200, gin.H{"user": fetchedUser})
+}
+
+// UpdateUser updates the detials of the user sending the request
+func (server *Server) UpdateUser(c *gin.Context) {
+	uidInterface, _ := c.Get("uid")
+	uid, err := strconv.ParseUint(fmt.Sprintf("%v", uidInterface), 10, 64)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": err})
+		return
+	}
+
+	user := models.User{}
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(500, gin.H{"error": err})
+		return
+	}
+
+	_, err = user.Update(server.DB, uid)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": err})
+		fmt.Println(err)
+		return
+	}
+
+	c.JSON(200, gin.H{"updated": user})
+	return
+
+}
+
+// DeleteUser removes the requesting user
+func (server *Server) DeleteUser(c *gin.Context) {
+	uidInterface, _ := c.Get("uid")
+	uid, err := strconv.ParseUint(fmt.Sprintf("%v", uidInterface), 10, 64)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": err})
+		return
+	}
+
+	user := models.User{}
+
+	_, err = user.Delete(server.DB, uid)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": err})
+		return
+	}
+
+	c.JSON(200, gin.H{"message": "success"})
+	return
+
 }
