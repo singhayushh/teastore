c70ee440b1dc019b2aa62d394ceffc5a674bee9f
Siddharth Pugalia <sidpug@users.noreply.github.com>
2020-11-15 18:35:32 +0530

    Frontend updated

---
+++ Frontend basic/Tesoretea/js/script.js
@@ -0,0 +1,16 @@
+ 
+
+// MOBIlE  NAV BAR
+function openNav() {
+    document.getElementById("myNav").style.width = "50%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+//   END OF THE MOBILE NAV
+
+
+// Products BOX
+ 
